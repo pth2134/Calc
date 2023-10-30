db51efdbe44fc125afadc0a26cf2f0784aabaee5
taehoon <xogns2134@gmail.com>
2023-10-30 14:26:57 +0900

    :~qw!

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -5,6 +5,45 @@ import java.util.*;
 public class Calc {
 
     static int run(String str) {
-        return 0;
+        String[] arr = str.split(" ");
+        ArrayList<String> list = new ArrayList<>(List.of(arr));
+        if(list.contains("(")) removePair(list);
+        return calc(list,0, list.size()-1);
+    }
+
+    static int calc(ArrayList<String> list, int start, int end){
+        int a = Integer.parseInt(list.get(start));
+        list.remove(start);
+        if(start>=end) {
+            return a;
+        }
+        String c = list.get(start);
+        list.remove(start);
+        if(c.equals("+")) {
+            return a + calc(list, start , end-2);
+        } else if(c.equals("-")){
+            return a - calc(list, start , end-2);
+        } else{
+            multOrDiv(list, start, a, c);
+            return calc(list,start, end-2);
+        }
+    }
+
+    static void multOrDiv(ArrayList<String> list, int start, int a, String c) {
+        int b = Integer.parseInt(list.get(start));
+        if(c.equals("*")){
+            list.set(start,a*b+"");
+        } else {
+            list.set(start,a/b+"");
+        }
+    }
+
+    static void removePair(ArrayList<String> list){
+        int now;
+        int left = list.indexOf("(");
+        list.remove(left);
+        if(list.contains("(")) removePair(list);
+        int right = list.indexOf(")");
+        list.set(left,calc(list, left,right-1)+"");
     }
 }
--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -2,18 +2,27 @@ package com.ll;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalcTest {
     @Test
     @DisplayName("Calc의 run메소드는 int를 리턴한다.")
-    void t1(){
-        int r = Calc.run("2*2");
+    void t1() {
+        int r = Calc.run("2 * 2");
     }
+
     @Test
     @DisplayName("10 + 10 = 20")
     void t2() {
         int rs = Calc.run("10 + 10");
         assertThat(rs).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("3 * 1 + ( 1 - ( 4 * 1 - ( 1 - 1 ) ) ) = 0")
+    void t3() {
+        int rs = Calc.run("3 * 1 + ( 1 - ( 4 * 1 - ( 1 - 1 ) ) )");
+        assertThat(rs).isEqualTo(0);
     }
 }
